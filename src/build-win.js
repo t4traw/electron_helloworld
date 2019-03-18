d3bfc17e55b35a8636e2123ace527d9642e9a9c3
Tatsuro Moriyama <t4traw@gmail.com>
2019-03-18 14:09:01 +0900

    exeにビルドするまで

---
+++ src/build-win.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const builder = require('electron-builder');
+const fs = require('fs');
+const packagejson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+builder.build({
+  platform: 'win',
+  config: {
+    'appId': `com.example.${packagejson.name}`,
+    'win': {
+      'target': 'portable',
+      "icon": "icon.ico",
+    },
+  },
+});
